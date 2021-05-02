5697d26f0dbc9524f4d3e7f9f36dba3247b1da6e
vweisscobos <v.weisscobos@gmail.com>
2021-05-02 15:15:50 -0300

    add index.js

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 const DimensionController = require('./controllers/Wind')
 
 routes.get('/', DimensionController.Wind)
+routes.get('/api', (req, res) => { return res.json({ resposta: "resposta" })})
 
 module.exports = routes;
