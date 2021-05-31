56a9cf9bfddb047989c73017a8b8b47dd571e881
Ronaldo de Oliveira <ronaldo.de.oliveira@outlook.com>
2021-05-30 23:45:03 -0300

    Add the cors

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ const server = express();
 const PORT = 5050;
 const routes = require('./routes')
 const path = require('path')
+const cors = require('cors')
 
+server.use(cors())
 server.set('view engine', 'ejs')
 
 server.set('views', path.join(__dirname, 'views'))
